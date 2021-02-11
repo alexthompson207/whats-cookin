ff1fa9111c3bd7c853d07eba23816118ea756115
Alex Thompson <ahthomps1@gmail.com>
2021-02-10 21:38:44 -0700

    Add function to fiter search by ingredient or recipe name

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,15 @@
 const recipeListCard = document.querySelector('.recipe-list');
 const recipesRepo = new RecipeRepo(recipeData);
+const searchBtn = document.querySelector('#searchRecipes');
+const searchInput = document.querySelector('.search-bar');
 
-function displayAllRecipeCards(recipesRepo) {
+var option = document.getElementsByTagName("option")[0];
+
+searchBtn.addEventListener('click', handleSearchDropDown)
+
+function displayAllRecipeCards(allRecipeData) {
   recipeListCard.innerHTML = '';
-  recipesRepo.recipes.forEach(recipe => {
+  allRecipeData.recipes.forEach(recipe => {
     recipeListCard.innerHTML +=
       `<div class='recipe-img-container'>
     <img class='recipe-img' src="${recipe.image}"
@@ -11,5 +17,29 @@ function displayAllRecipeCards(recipesRepo) {
       <p class='recipe-name'>${recipe.name}</p>
   </div>`
   })
+}
+
+function searchByIngrients() {
+  // event.preventDefault();
+  const searchResultRecipes = recipesRepo.filterRecipesByIngredients(ingredientsData, searchInput.value);
+  console.log(searchResultRecipes);
+  displayAllRecipeCards({ recipes: searchResultRecipes });
+}
+
+function searchByRecipeName() {
+  // event.preventDefault();
+  const searchResultName = recipesRepo.filterRecipesByName(searchInput.value);
+  console.log(searchResultName);
+  displayAllRecipeCards({ recipes: [searchResultName] });
+}
+
+function handleSearchDropDown(event) {
+  event.preventDefault();
+  let searchBy = document.getElementById('search-recipe-select').value;
+  if (searchBy === 'recipe') {
+    searchByRecipeName(event);
+  } else if (searchBy === 'ingredient') {
+    searchByIngrients(event);
+  }
 }
 
