357470007546224910dd399477d59bec38dd8a0c
Alex Thompson <ahthomps1@gmail.com>
2021-02-13 21:46:59 -0700

    Add test outline for evaulating ingredients of a recipe against pnatry items for a user

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const Pantry = require('../src/Pantry');
 describe.only('Pantry', () => {
-  let pantry1, pantry2, contents1, contents2;
+  let pantry1, pantry2, contents1, contents2, recipe1;
   beforeEach(() => {
     contents1 = [{
       "ingredient": 1,
@@ -37,22 +37,63 @@ describe.only('Pantry', () => {
         "ingredient": 10,
         "amount": 3
       }];
+    recipe1 = recipe1 = {
+      id: 991136,
+      image: 'https://spoonacular.com/recipeImages/991136-556x370.jpg',
+      ingredients: [
+        {
+          id: 1,
+          quantity: {
+            amount: 0.25,
+            unit: 'cup',
+          },
+        },
+        {
+          id: 2,
+          quantity: {
+            amount: 12,
+            unit: 'unit',
+          },
+        },
+      ],
+      instructions: [
+        { instruction: 'step 1', number: 1 },
+        { instruction: 'step 2', number: 2 },
+        { instruction: 'step 3', number: 3 },
+      ],
+      name: 'Buffalo Chicken Example',
+      tags: ['lunch', 'main course', 'main dish', 'dinner'],
+    };
     pantry1 = new Pantry(contents1);
     pantry2 = new Pantry(contents2);
   });
+
   it('should be a function', () => {
     expect(Pantry).to.be.a('function');
   });
+
   it('should be an instance of Pantry', () => {
     expect(pantry1).to.be.an.instanceof(Pantry);
   });
+
   it('should be able to hold contents of ingredients and amounts', () => {
     expect(pantry1.pantry).to.deep.equal(contents1);
   });
+
   it('should hold an ingredient id in the contents', () => {
     expect(pantry1.pantry[2].ingredient).to.equal(5);
   });
+
   it('should hold an amount in the contents', () => {
     expect(pantry1.pantry[1].amount).to.equal(3);
+  });
+
+  it('should be able to evaluate what ingredients are needed to cook a recipe', () => {
+    const result = pantry1.evaluateIngredientsNeeded(recipe1);
+
+    expect(result).to.deep.equal([{ id: 2, amountNeeded: 9 }]);
+    expect(result).to.deep.equal([
+      { id: 2, quantity: { amount: 9, unit: 'unit' } }
+    ]);
   });
 })
