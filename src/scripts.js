06a2c25ecb9a01d42aa81baae22cee1d4a24975f
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-17 15:40:18 -0700

    Fix event handler for favorite img click

--- src/scripts.js
+++ src/scripts.js
@@ -65,7 +65,7 @@ function displayFavoriteRecipeCards(favoriteRecipes) {
     recipeListCard.innerHTML +=
       `<div class='recipe-img-container'>
       <svg class="remove-icon" id="removeIcon" xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><title>Close Circle</title><path d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z' fill='none' stroke='currentColor' stroke-miterlimit='10' stroke-width='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M320 320L192 192M192 320l128-128'/></svg>
-    <img class='favorite-recipe-img' id="${recipe.id}" src="${recipe.image}"
+    <img class='recipe-img favorite-recipe-img' id="${recipe.id}" src="${recipe.image}"
       alt="${recipe.name}">
       <p class='recipe-name'>${recipe.name}</p>
   </div>`
